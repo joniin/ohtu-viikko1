ec58d61334f5422058cd91c3627ca80244b37322
joniin <jouko.niinimaki@cs.helsinki.fi>
2014-09-07 20:20:23 +0300

    testi korjattu taas

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -98,6 +98,6 @@ public class VarastoTest {
     public void otetaanLiikaa(){
         varasto.otaVarastosta(25);
         
-        assertEquals(4, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
 }
